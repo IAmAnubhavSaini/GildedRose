414b3f260d547f181afdec792a260561cf6dada8
Anubhav Saini <IAmAnubhavSaini@GMail.com>
2014-12-17 22:23:37 +0530

    DESIGN - REFACTORING - GILDED ROSE - Added a visual studio test project.

---
+++ VSUnitTests/UpdateTests.cs
@@ -0,0 +1,27 @@
+﻿using GildedRose.Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSUnitTests
+{
+    [TestClass]
+    public class UpdateTests
+    {
+
+
+        [TestMethod]
+        public void StandardItemSellInShouldDecreaseByOneAfterOneDayUpdate()
+        {
+            var item = new Item { Name = "standard item", SellIn = 1, Quality = 1 }.UpdateQuality();
+            Assert.AreEqual(0, item.SellIn);
+        }
+    }
+
+    public static class TestHelper
+    {
+        private static Program program = new Program();
+        public static Item UpdateQuality(this Item item)
+        {
+            return program.UpdateQuality(item);
+        }
+    }
+}
