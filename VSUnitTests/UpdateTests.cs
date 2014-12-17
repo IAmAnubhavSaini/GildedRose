9a72fbcf4da0df3e1579e6da1602356696e10db1
Anubhav Saini <IAmAnubhavSaini@GMail.com>
2014-12-17 23:03:05 +0530

    DESIGN - REFACTORING - GILDED ROSE - All the tests pass. Yay, and there are ton of them.
    They might not cover all the scenarios still, but meh and good start.

--- VSUnitTests/UpdateTests.cs
+++ VSUnitTests/UpdateTests.cs
@@ -6,22 +6,95 @@ namespace VSUnitTests
     [TestClass]
     public class UpdateTests
     {
-
-
         [TestMethod]
         public void StandardItemSellInShouldDecreaseByOneAfterOneDayUpdate()
         {
-            var item = new Item { Name = "standard item", SellIn = 1, Quality = 1 }.UpdateQuality();
+            var item = new StandardItem { Name = "standard item", SellIn = 1, Quality = 1 };
+            item.Update();
             Assert.AreEqual(0, item.SellIn);
         }
-    }
 
-    public static class TestHelper
-    {
-        private static Program program = new Program();
-        public static Item UpdateQuality(this Item item)
+        [TestMethod]
+        public void BackstagePassesShouldIncreaseQualityWhenConcertIsIn11Days()
         {
-            return program.UpdateQuality(item);
+            var item = new BackstagePassItem { Name = "back stage pass", SellIn = 11, Quality = 40 };
+            item.Update();
+            Assert.AreEqual(41, item.Quality);
+        }
+
+        [TestMethod]
+        public void BackstagePassesShouldIncreaseQualityWhenConcertIsIn7Days()
+        {
+            var item = new BackstagePassItem { Name = "back stage pass", SellIn = 7, Quality = 40 };
+            item.Update();
+            Assert.AreEqual(42, item.Quality);
+        }
+
+        [TestMethod]
+        public void BackstagePassesShouldIncreaseQualityWhenConcertIsIn3Days()
+        {
+            var item = new BackstagePassItem { Name = "back stage pass", SellIn = 3, Quality = 40 };
+            item.Update();
+            Assert.AreEqual(43, item.Quality);
+        }
+
+        [TestMethod]
+        public void BackstagePassesShouldLooseAllQualityWhenExpired()
+        {
+            var item = new BackstagePassItem { Name = "back stage pass", SellIn = 0, Quality = 40 };
+            item.Update();
+
+            Assert.AreEqual(0, item.Quality);
+        }
+
+        [TestMethod]
+        public void ConjuredItemQualityShouldDecreaseTwiceAsFastAsStandardItem()
+        {
+            var sellin = 10;
+            var quality = 40;
+            var item = new StandardItem { Name = "standard item", SellIn = sellin, Quality = quality };
+            var conjureItem = new ConjuredItem { Name = "conjured item", SellIn = sellin, Quality = quality };
+
+
+            StandardVersusConjuredQualityDegradationBeforeSellInIsZero(item, conjureItem);
+            StandardVersusConjuredQualityDegradationAfterSellInIsZero(item, conjureItem);
+        }
+
+        private static void StandardVersusConjuredQualityDegradationBeforeSellInIsZero(StandardItem item,
+            ConjuredItem conjureItem)
+        {
+            var sellin = 10;
+            var itemQuality = item.Quality;
+            var conjuredItemQuality = conjureItem.Quality;
+
+            while (sellin-- > 0)
+            {
+                item.Update();
+                conjureItem.Update();
+                Assert.AreEqual(itemQuality - 1, item.Quality);
+                Assert.AreEqual(conjuredItemQuality - 2, conjureItem.Quality);
+
+                itemQuality--;
+                conjuredItemQuality -= 2;
+            }
+        }
+
+        private static void StandardVersusConjuredQualityDegradationAfterSellInIsZero(StandardItem item,
+            ConjuredItem conjureItem)
+        {
+            var sellin = 0;
+            var itemQuality = item.Quality;
+            var conjuredItemQuality = conjureItem.Quality;
+            while (sellin-- > -10)
+            {
+                item.Update();
+                conjureItem.Update();
+                Assert.AreEqual(itemQuality - 2, item.Quality);
+                Assert.AreEqual(conjuredItemQuality - 4, conjureItem.Quality);
+
+                itemQuality -= 2;
+                conjuredItemQuality -= 4;
+            }
         }
     }
 }
--- src/GildedRose.Tests/UpdateItemsTests.cs
+++ src/GildedRose.Tests/UpdateItemsTests.cs
@@ -1,104 +1,96 @@
-﻿using System.Collections.Generic;
-using GildedRose.Console;
+﻿using GildedRose.Console;
 using NUnit.Framework;
 
 namespace GildedRose.Tests
 {
-    public class when_i_update_items
+    public class WhenIUpdateItems
     {
         [TestFixture]
-        public class with_a_standard_item
+        public class WithAStandardItem
         {
 
             [Test]
             public void it_should_decrement_sell_in()
             {
-                var item = new Item {Name = "standard item", SellIn = 1, Quality = 1 }.UpdateQuality();
+                var item = new StandardItem{Name = "standard item", SellIn = 1, Quality = 1 }.UpdateQuality();
                 Assert.That(item.SellIn, Is.EqualTo(0));
             }
 
             [Test]
             public void it_should_decrement_quality()
             {
-                var item = new Item { Name = "standard item", SellIn = 1, Quality = 1 }.UpdateQuality();
+                var item = new StandardItem { Name = "standard item", SellIn = 1, Quality = 1 }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(0));
             }
 
             [Test]
             public void when_the_sell_in_has_passed_it_should_decrement_quality_twice_as_fast()
             {
-                var item = new Item { Name = "standard item", SellIn = 0, Quality = 2 }.UpdateQuality();
+                var item = new StandardItem { Name = "standard item", SellIn = 0, Quality = 2 }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(0));
             }
 
             [Test]
             public void quality_should_never_be_negative()
             {
-                var item = new Item { Name = "standard item", SellIn = 1, Quality = 0 }.UpdateQuality();
+                var item = new StandardItem { Name = "standard item", SellIn = 1, Quality = 0 }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(0));
             }
         }
 
         [TestFixture]
-        public class with_a_legendary_item
+        public class WithALegendaryItem
         {
        
             [Test]
             public void it_should_not_decrease_in_quality()
             {
-                var item = new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 1, SellIn = 1 }.UpdateQuality();
+                var item = new LegendaryItem { Name = "Sulfuras, Hand of Ragnaros", Quality = 1, SellIn = 1 }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(1));
             }
 
             [Test]
             public void it_should_not_decrease_sell_in()
             {
-                var item = new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 1, SellIn = 1 }.UpdateQuality();
+                var item = new LegendaryItem { Name = "Sulfuras, Hand of Ragnaros", Quality = 1, SellIn = 1 }.UpdateQuality();
                 Assert.That(item.SellIn, Is.EqualTo(1));
             }
         }
 
         [TestFixture]
-        public class with_a_quality_aged_item
+        public class WithAgedBrieItem
         {
 
             [Test]
             public void it_should_increase_in_quality()
             {
-                var item = new Item { Name = "Aged Brie", Quality = 1, SellIn = 1 }.UpdateQuality();
+                var item = new AgedBrieItem { Name = "Aged Brie", Quality = 1, SellIn = 1 }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(2));
             }
 
             [Test]
             public void quality_should_never_increase_beyond_fifty()
             {
-                var item = new Item { Name = "Aged Brie", Quality = 50, SellIn = 1 }.UpdateQuality();
+                var item = new AgedBrieItem { Name = "Aged Brie", Quality = 50, SellIn = 1 }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(50));
             }
 
             [Test]
             public void sell_in_should_still_decrement_normally()
             {
-                var item = new Item { Name = "Aged Brie", Quality = 1, SellIn = 1 }.UpdateQuality();
+                var item = new AgedBrieItem { Name = "Aged Brie", Quality = 1, SellIn = 1 }.UpdateQuality();
                 Assert.That(item.SellIn, Is.EqualTo(0));
-            }
-
-            [Test]
-            public void quality_doubles_once_sell_in_is_zero()
-            {
-                var item = new Item { Name = "Aged Brie", Quality = 1, SellIn = 0 }.UpdateQuality();
-                Assert.That(item.Quality, Is.EqualTo(3));
             }
         }
 
         [TestFixture]
-        public class with_an_event_item
+        public class WithABackstagePassItem
         {
 
             [Test]
             public void it_should_increase_in_quality()
             {
-                var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 11 }.UpdateQuality();
+                var item = new BackstagePassItem { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 11 }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(2));
             }
 
@@ -109,7 +101,7 @@ namespace GildedRose.Tests
             [TestCase(6)]
             public void it_should_increase_quality_by_two_when_there_are_ten_or_less_days_left(int daysLeft)
             {
-                var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = daysLeft }.UpdateQuality();
+                var item = new BackstagePassItem { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = daysLeft }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(3));
             }
 
@@ -120,21 +112,21 @@ namespace GildedRose.Tests
             [TestCase(1)]
             public void it_should_increase_quality_by_three_when_there_are_five_days_left(int daysLeft)
             {
-                var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = daysLeft }.UpdateQuality();
+                var item = new BackstagePassItem { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = daysLeft }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(4));
             }
 
             [Test]
             public void it_should_drop_quality_to_zero_when_sell_in_has_passed()
             {
-                var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 0 }.UpdateQuality();
+                var item = new BackstagePassItem { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 0 }.UpdateQuality();
                 Assert.That(item.Quality, Is.EqualTo(0));
             }
 
             [Test]
             public void sell_in_should_decrement_normally()
             {
-                var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 1 }.UpdateQuality();
+                var item = new BackstagePassItem { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 1 }.UpdateQuality();
                 Assert.That(item.SellIn, Is.EqualTo(0));
             }
         }
@@ -144,9 +136,7 @@ namespace GildedRose.Tests
     {
         public static Item UpdateQuality(this Item item)
         {
-            var program = new Program ();
-            program.UpdateQuality();
-
+            item.Update();
             return item;
         }
     }
